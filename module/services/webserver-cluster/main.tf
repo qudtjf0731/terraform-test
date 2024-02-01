7c80a0241ef40a0c721961174430b57e75aa71dc
qudtjf0731 <qudtjf0731@naver.com>
2024-02-01 08:42:38 +0000

    module

--- module/services/webserver-cluster/main.tf
+++ module/services/webserver-cluster/main.tf
@@ -1,18 +1,13 @@
-provider "aws" {
-  region = "ap-northeast-2"
-}
-
-
 resource "aws_lb" "example" {
-  name = "terraform-asg-example"
+  name = "${var.cluster_name}-example"
   load_balancer_type = "application"
   subnets = data.aws_subnets.default.ids
-  security_groups = [aws_security_group.alb.id] 
+  security_groups = [aws_security_group.alb.id]
 }
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port = 80
+  port = local.http_port
   protocol = "HTTP"
   default_action {
     type = "fixed-response"
@@ -29,7 +24,7 @@ resource "aws_lb_listener_rule" "asg" {
   priority = 100
   condition {
     path_pattern {
-      values = ["*"] 
+      values = ["*"]
     }
   }
   action {
@@ -42,7 +37,7 @@ resource "aws_lb_listener_rule" "asg" {
 
 
 resource "aws_lb_target_group" "asg" {
-  name = "terraform-asg-example"
+  name = "${var.cluster_name}-asg"
   port = var.server_port
   protocol = "HTTP"
   vpc_id = data.aws_vpc.default.id
@@ -58,21 +53,26 @@ resource "aws_lb_target_group" "asg" {
 }
 
 resource "aws_security_group" "alb" {
-  name = "terraform-example-alb"
-  
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  name = "${var.cluster_name}-alb"
 }
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.alb.id
+  from_port = local.http_port
+  to_port = local.http_port
+  protocol = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type = "egress"
+  security_group_id = aws_security_group.alb.id
+  from_port = local.any_port
+  to_port = local.any_port
+  protocol = local.any_protocol
+  cidr_blocks = local.all_ips
+
 
 
 
@@ -81,13 +81,13 @@ resource "aws_security_group" "alb" {
 resource "aws_launch_configuration" "example" {
   image_id = "ami-0f3a440bbcff3d043"
   instance_type = "t3.micro"
-  security_groups = [aws_security_group.instance.id] 
-  user_data = <<-E0F
-    #!/bin/bash
-    echo "Hello, World" > index.html
-    nohup busybox httpd -f -p {var.server_port}  &
-    E0F
- 
+  security_groups = [aws_security_group.instance.id]
+  user_data = templatefile ("${path.module}/user-data.sh", {
+    server_port = var.server_port
+    db_address = data.terraform_remote_state.db.outputs.address
+    db_port = data.terraform_remote_state.db.outputs.port
+  })
+
   lifecycle {
     create_before_destroy = true
   }
@@ -98,44 +98,24 @@ resource "aws_autoscaling_group" "example" {
   vpc_zone_identifier = data.aws_subnets.default.ids
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
-  min_size = 2
-  max_size = 10
+  min_size = var.min_size
+  max_size = var.max_size
   tag {
     key = "Name"
-    value = "terraform-asg-example"
+    value = "${var.cluster_name}-example"
     propagate_at_launch = true
-  } 
+  }
 }
+
 
 resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-  
+  name = "${var.cluster_name}-instance"
+
   ingress {
-    from_port = var.server_port 
-    to_port = var.server_port
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    from_port = local.http_port
+    to_port = local.http_port
+    protocol = local.tcp_protocol
+    cidr_blocks = local.all_ips
   }
 }
 
-variable "server_port" {
-  description = "server port for http request"
-  type = number
-  default = 8080
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-output "alb_dns_name" {
-  value = aws_lb.example.dns_name
-  description = "The domain name of the load balancer"
-}
